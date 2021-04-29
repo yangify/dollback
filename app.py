f05e51dcc4286eab620edea85f18206f391db39b
hongyang <hong.yang.code@gmail.com>
2021-04-29 16:04:49 +0800

    added upload

--- app.py
+++ app.py
@@ -10,5 +10,23 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/upload_page')
+def upload_page():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return 'Files required'
+
+    if not os.path.exists("./resources/apk"):
+        os.makedirs("./resources/apk")
+
+    file = request.files['file']
+    file.save('./resources/apk/' + file.filename)
+    return 'Success'
+
+
 if __name__ == '__main__':
     app.run()

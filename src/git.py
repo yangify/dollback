dfbba70717e93f9ea42f15db8fd3aa145b125395
hongyang <hong.yang.code@gmail.com>
2021-06-02 14:09:28 +0800

    removed extraction and added commit

--- src/git.py
+++ src/git.py
@@ -1,22 +1,8 @@
-import subprocess
+import os
 
 from flask import current_app as app
 
 
-def commit_and_push(filename):
-    # TODO
-    print(filename)
-
-
-def create_repository(repository_name):
-    cmd = app.config['CREATE_REPOSITORY_CMD'].format(repository_name=repository_name).split()
-    output = subprocess.run(cmd)
-    return output.returncode == 0
-
-
-def clone(repository_name):
-    username = app.config['CODECOMMIT_USERNAME']
-    password = app.config['CODECOMMIT_PASSWORD']
-    cmd = app.config['CLONE_REPOSITORY_CMD'].format(username=username, password=password, repository_name=repository_name).split()
-    output = subprocess.run(cmd)
-    return output.returncode == 0
+def commit(filename):
+    cmd = app.config['COMMIT'].format(filename=filename)
+    os.system(cmd)
--- src/tasks.py
+++ src/tasks.py
@@ -2,18 +2,16 @@ from celery import shared_task
 
 from flask import current_app as app
 from src.decompiler import decompile
-from src.extractor import extract_and_save
-from src.git import commit_and_push
+from src.git import commit
 
 
 def process(filename):
     decompilers = app.config['DECOMPILERS']
     for decompiler in decompilers:
-        process_each.delay(filename, decompiler)
+        process_each(filename, decompiler)
+    commit(filename)
 
 
 @shared_task(name='tasks.process')
 def process_each(filename, decompiler):
     decompile(filename, decompiler)
-    extract_and_save(filename, decompiler)
-    commit_and_push(filename)
